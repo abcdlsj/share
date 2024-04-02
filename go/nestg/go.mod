12c824cef9ecd0e119392dc82f0fd9a75ffc2daa
abcdlsj <lisongjianshuai@gmail.com>
2024-04-02 23:22:57 +0800

    nestg: support more

--- go/nestg/go.mod
+++ go/nestg/go.mod
@@ -2,4 +2,7 @@ module github.com/abcdlsj/share/go/nestg
 
 go 1.20
 
-require github.com/abcdlsj/cr v0.0.0-20230814105742-5bf617e8b59e
+require (
+	github.com/abcdlsj/cr v0.0.0-20230814105742-5bf617e8b59e
+	golang.org/x/mod v0.16.0
+)
--- go/nestg/main.go
+++ go/nestg/main.go
@@ -1,139 +1,186 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/abcdlsj/cr"
+	"golang.org/x/mod/modfile"
 )
 
 type Identifier struct {
-	Name      string
-	Docker    DockerFile
-	BuildEnvs []string
+	Name   string
+	Docker DockerFile
 }
 
 type DockerFile struct {
-	FromBase   string
-	ExposePort string
-	ExecCmd    string
-	Runs       []string
+	Stages []Stage
+	Execs  []string
+}
+
+type Stage struct {
+	From   string
+	Builds []string
+	Expose string
+}
+
+func (s *Stage) String() string {
+	var sb strings.Builder
+	sb.WriteString(fmt.Sprintf("FROM %s\n", s.From))
+	for _, v := range s.Builds {
+		sb.WriteString(fmt.Sprintf("%s\n", v))
+	}
+
+	if s.Expose != "" {
+		sb.WriteString(fmt.Sprintf("EXPOSE %s\n", s.Expose))
+	}
+
+	return sb.String()
 }
 
 func (d *DockerFile) String() string {
 	var sb strings.Builder
-	sb.WriteString(fmt.Sprintf("FROM %s\n", d.FromBase))
-	for _, v := range d.Runs {
-		sb.WriteString(fmt.Sprintf("%s\n", v))
+
+	for _, v := range d.Stages {
+		sb.WriteString(v.String() + "\n")
 	}
 
-	if d.ExposePort != "" {
-		sb.WriteString(fmt.Sprintf("EXPOSE %s\n", d.ExposePort))
+	sb.WriteString("CMD [")
+
+	for i, v := range d.Execs {
+		sb.WriteString(fmt.Sprintf("\"%s\"", v))
+		if i != len(d.Execs)-1 {
+			sb.WriteString(", ")
+		}
 	}
 
-	sb.WriteString(fmt.Sprintf(d.ExecCmd, "%s"))
+	sb.WriteString("]")
+
 	return sb.String()
 }
 
 var (
 	exposePort string
-	goLdflag   string
-	dImgName   string
-
-	AlapineI = Identifier{
-		Name: "alpine",
-		Docker: DockerFile{
-			FromBase:   "alpine:latest",
-			Runs:       vec("RUN mkdir /app", "WORKDIR /app", "COPY . ."),
-			ExecCmd:    "CMD [\"/app/%s\"]",
-			ExposePort: exposePort,
-		},
-		BuildEnvs: vec("CGO_ENABLED=0", "GOOS=linux"),
-	}
+	ldflags    string
+	imgname    string
+	execFlags  string
+	debug      = false
 )
 
-func getBinaryName() (string, error) {
+func genBuildCmd(binName, ldflags string) string {
+	var sb strings.Builder
+	sb.WriteString("RUN ")
+
+	if ldflags == "" {
+		ldflags = "-s -w"
+	}
+
+	sb.WriteString(fmt.Sprintf("go build -ldflags=\"%s\" -trimpath -o /dist/%s .", ldflags, binName))
+
+	return sb.String()
+}
+
+func getBinaryName() string {
 	dir, err := os.Getwd()
 	if err != nil {
-		return "", err
+		return time.Now().Format("20060102150405") + "-" + "app"
 	}
 
-	file, err := os.OpenFile(dir+"/go.mod", os.O_RDONLY, 0644)
+	data, err := os.ReadFile(filepath.Join(dir, "go.mod"))
 	if err != nil {
-		if os.IsNotExist(err) {
-			return path.Base(dir), nil
-		}
-		return "", err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		modPath := strings.TrimSpace(scanner.Text()[7:])
-		return path.Base(modPath), nil
+		return path.Base(dir)
 	}
 
-	return path.Base(dir), nil
+	modf, err := modfile.Parse("go.mod", data, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	mods := strings.Split(modf.Module.Mod.Path, "/")
+
+	return mods[len(mods)-1]
 }
 
 func init() {
-	flag.StringVar(&exposePort, "p", "", "port")
-	flag.StringVar(&dImgName, "i", "", "image name")
-	flag.StringVar(&goLdflag, "b", "", "go build flags")
+	flag.StringVar(&exposePort, "port", "", "port")
+	flag.StringVar(&imgname, "img", "", "image name")
+	flag.StringVar(&ldflags, "ldflags", "", "go build flags")
+	flag.StringVar(&execFlags, "execflags", "", "exec flags")
+	flag.BoolVar(&debug, "debug", false, "debug")
 }
 
-func getIdentifier() Identifier {
-	return AlapineI
+func getUserName() string {
+	return os.Getenv("USER")
 }
 
 func main() {
 	flag.Parse()
-	apapine := getIdentifier()
 
-	binName, err := getBinaryName()
-	if err != nil {
-		fmt.Printf("Scan binary file failed: %s\n", cr.PLRed(err.Error()))
+	binName := getBinaryName()
+
+	ident := Identifier{
+		Name: "golang:alpine",
+		Docker: DockerFile{
+			Stages: []Stage{
+				{
+					From: "golang:1-alpine AS builder",
+					Builds: vec(
+						"RUN apk add --no-cache build-base",
+						"WORKDIR /build",
+						"COPY . .",
+						genBuildCmd(binName, ldflags),
+						"RUN ldd /dist/"+binName+" | tr -s [:blank:] '\\n' | grep ^/ | xargs -I % install -D % /dist/%",
+						"RUN ln -s ld-musl-x86_64.so.1 /dist/lib/libc.musl-x86_64.so.1",
+					),
+				},
+				{
+					From: "scratch",
+					Builds: vec(
+						"COPY --from=builder /dist /",
+					),
+					Expose: exposePort,
+				},
+			},
+			Execs: []string{
+				"/" + binName,
+			},
+		},
 	}
 
-	if dImgName == "" {
-		dImgName = "nestg" + "/" + binName + ":" + time.Now().Format("20060102150405")[8:]
+	execFlags := strings.Split(execFlags, " ")
+	for _, v := range execFlags {
+		if v == "" {
+			continue
+		}
+		ident.Docker.Execs = append(ident.Docker.Execs, fmt.Sprintf("\"%s\"", v))
 	}
 
-	fmt.Printf("Identifier: %s, Binary: %s, Image: %s\n", cr.PLBlue(apapine.Name), cr.PLBlue(binName), cr.PLBlue(dImgName))
+	if imgname == "" {
+		imgname = getUserName() + "/" + binName + ":" + time.Now().Format("20060102150405")[8:]
+	}
 
-	tmpf, err := os.CreateTemp("", "nestg-*.dockerfile")
+	fmt.Printf("Identifier: %s, Binary: %s, Image: %s\n", cr.PLBlue(ident.Name), cr.PLBlue(binName), cr.PLBlue(imgname))
+
+	tmpf, err := os.CreateTemp("", fmt.Sprintf("%s-*.dockerfile", binName))
 	if err != nil {
 		fmt.Printf("Temp file create error: %s\n", cr.PLRed(err.Error()))
 		return
 	}
 
-	data := fmt.Sprintf(apapine.Docker.String(), binName)
+	fmt.Printf("Temp dockerfile: %s\n", cr.PLYellow(tmpf.Name()))
 
-	tmpf.WriteString(data)
+	tmpf.WriteString(ident.Docker.String())
 	defer os.Remove(tmpf.Name())
 
-	fmt.Printf("Dockerfile:\n%s\n", cr.PLYellow(data))
+	fmt.Printf("Dockerfile content:\n%s\n", cr.PLYellow(ident.Docker.String()))
 
-	cmd := exec.Command("go", "build", "-o", binName, ".")
-	if goLdflag != "" {
-		cmd = exec.Command("go", "build", "-ldflags", goLdflag, "-o", binName, ".")
-	}
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Env = os.Environ()
-	cmd.Env = append(cmd.Env, apapine.BuildEnvs...)
-	if err := cmd.Run(); err != nil {
-		fmt.Printf("Build error: %s\n", cr.PLRed(err.Error()))
-		return
-	}
-
-	cmd = exec.Command("docker", "build", "-t", dImgName, "-f", tmpf.Name(), ".")
+	cmd := exec.Command("docker", "build", "-t", imgname, "-f", tmpf.Name(), ".")
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
@@ -141,11 +188,17 @@ func main() {
 		return
 	}
 
-	if exposePort != "" {
-		fmt.Printf("Run: %s\n", cr.PLYellow("docker run -it --rm -p "+"port:"+exposePort+" "+dImgName))
+	if debug {
+		fmt.Printf("Run: %s\n", cr.PLYellow("docker run -it --rm "+imgname))
 		return
 	}
-	fmt.Printf("Run: %s\n", cr.PLYellow("docker run -it --rm "+dImgName))
+
+	if exposePort != "" {
+		fmt.Printf("Run: %s\n", cr.PLYellow("docker run -d --rm -p "+"<hostport>:"+exposePort+" "+imgname))
+		return
+	}
+
+	fmt.Printf("Run: %s\n", cr.PLYellow("docker run -d --rm "+imgname))
 }
 
 func vec(s ...string) []string {
